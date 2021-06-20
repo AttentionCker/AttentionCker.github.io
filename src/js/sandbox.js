499cb2ca80c0d5a9117f972756fc0ed40dcf8ce0
AttentionCker <attentioncker@gmail.com>
2021-06-21 01:23:24 +0530

    [MAJOR] Navbar Connected with Page Sections. They dynamically change in accordance.

--- src/js/sandbox.js
+++ src/js/sandbox.js
@@ -9,6 +9,10 @@ function closeNav() {
     document.getElementById("main").style.marginLeft = "0";
 }
 
+
+
+let scrollAnimEnabled = true;
+
 function sidenavbar() {
     let sidenavElement = $(".sidenav-element");
     let activeElement = $(".active");
@@ -22,6 +26,7 @@ function sidenavbar() {
     });
     sidenavElement.on({
         click: function() {
+            scrollAnimEnabled = false;
             $(".active").removeClass("active");
             $(this).addClass("active");
             activeElementHighlighter.css({
@@ -31,12 +36,35 @@ function sidenavbar() {
                 "width": String(Number($(".active").innerWidth()) * 0.8) + "px",
                 "margin-left": String(Number($(".active").innerWidth()) * 0.2) + "px"
             });
+            setTimeout(() => { scrollAnimEnabled = true; }, 550);
         }
     });
 }
 
+$(".active-nav-item-onscroll").scroll(function() {
+    let scrollDistance = $(".active-nav-item-onscroll").scrollTop();
+    let vh = $(window).height();
+    let i = Math.round(scrollDistance / vh);
+    console.log(i);
+    if (scrollAnimEnabled) {
+        $(".sidenav-element.active").removeClass("active");
+        $(".sidenav-element").eq(i).addClass("active");
+        let activeElement = $(".sidenav-element.active");
+        let activeElementHighlighter = $("#active-elem-highlighter");
+        activeElementHighlighter.css({
+            "top": $(".active").position().top + "px",
+            "left": $(".active").position().left + "px",
+            "height": $(".active").innerHeight() + "px",
+            "width": String(Number($(".active").innerWidth()) * 0.8) + "px",
+            "margin-left": String(Number($(".active").innerWidth()) * 0.2) + "px"
+        });
+    }
+});
+
+
 $(document).ready(function() {
     sidenavbar();
+    var callbacks = $.Callbacks();
 });
 
 $(window).resize(function() {
